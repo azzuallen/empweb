051dc8d0c3c62703949616ef00af847b294e27dd
Azhar <azzuhussain06@gmail.com>
2023-07-19 14:06:32 +0530

    ignore back folders

---
+++ typescript data/app.ts
@@ -0,0 +1,37 @@
+// const num  = [1, 2, 3,7]; // inferred to type number[]
+// num.push(4);
+
+// console.warn(num); 
+// var a = ("aaa na ")
+//console.warn(a);
+
+//interface  
+// 
+//  interface interfaceName {
+//     name: string,
+//     age: number
+// }
+// var obj:interfaceName= {
+
+//     name: "azzu",
+//     age : 26
+// }
+// console.warn(objj)
+
+// any type - can assign any data type
+// var objj:any= {
+//  name: "azzu",
+//     age : 26
+// }
+// console.warn(objj)
+
+
+// union  type - multiple data can assign data type
+// var  data:string|number = "azzu"
+// data = 26;
+// console.warn(data)
+
+// // union  type - multiple data can assign data type
+var  data:string|number = "azzu"
+data = 26;
+console.warn(data)
